ac141752a56fdcee2cf4ba05b1a562d5a5b18087
dev-ayaa <ayaaakinleye@gmail.com>
2023-06-08 12:46:43 +0100

    change the return type for the routes function

--- routes.go
+++ routes.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"net/http"
+	_"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/yusuf/mailapp/handlers"
 )
 
-func Routes(lg handlers.Logic) http.Handler {
+func Routes(lg handlers.Logic) *chi.Mux {
 	mux := chi.NewRouter()
+
 	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
-	mux.Use(middleware.RealIP)
 
 	mux.Get("/", lg.Home())
 	mux.Post("/api/submit", lg.GetSubscriber())
 	mux.Post("/api/send", lg.SendMail())
+
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/", lg.Home())
+	// mux.HandleFunc("/api/submit", lg.GetSubscriber())
+	// mux.HandleFunc("/api/send", lg.SendMail())
 
 	return mux
 }
