d24ed0dc9447a5fff80ed1a5727e355e31989066
akinbyte <ayaaakinleye@gmail.com>
2023-06-18 22:00:18 +0100

    add handler to render static file

--- routes.go
+++ routes.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/akinbyte/mailapp/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/akinbyte/mailapp/handlers"
 )
 
 func Routes(lg handlers.Logic) *chi.Mux {
 	mux := chi.NewRouter()
 
+	// default middleware to logger requests and avoid server shutting down
 	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
 
+	// setting up endpoints to respective handlers
 	mux.Get("/", lg.Home())
 	mux.Post("/api/submit", lg.GetSubscriber())
 	mux.Post("/api/send", lg.SendMail())
 
-	
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	// mux := http.NewServeMux()
 	// mux.HandleFunc("/", lg.Home())
 	// mux.HandleFunc("/api/submit", lg.GetSubscriber())
